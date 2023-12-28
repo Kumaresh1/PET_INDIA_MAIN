02295aa6c5041dab046843b044a1ea0886ee95d5
Kumaresh <kumareshappu12@gmail.com>
2023-12-28 13:36:46 +0530

    Animation fix and mail id link changed

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,20 @@
-jQuery(function($) {
-  
-  var doAnimations = function() {
-    
+jQuery(function ($) {
+  var doAnimations = function () {
     var offset = $(window).scrollTop() + $(window).height(),
-        $animatables = $('.animatable');
-    
+      $animatables = $(".animatable");
+
     if ($animatables.length == 0) {
-      $(window).off('scroll', doAnimations);
+      $(window).off("scroll", doAnimations);
     }
-    
-        $animatables.each(function(i) {
-       var $animatable = $(this);
-            if (($animatable.offset().top + $animatable.height() - 20) < offset) {
-        $animatable.removeClass('animatable').addClass('animated');
-            }
+
+    $animatables.each(function (i) {
+      var $animatable = $(this);
+      if ($animatable.offset().top + $animatable.height() - 100 < offset) {
+        $animatable.removeClass("animatable").addClass("animated");
+      }
     });
+  };
 
-    };
-  
-    $(window).on('scroll', doAnimations);
-  $(window).trigger('scroll');
-
+  $(window).on("scroll", doAnimations);
+  $(window).trigger("scroll");
 });
-
